36fadcf395e6facd2c0456624fc99cad624ce6d2
siyangye <siyang.ye0825@gmail.com>
2022-12-19 16:03:28 -0800

    debug outside #,(0,0)

--- lab 4 flood fill.py
+++ lab 4 flood fill.py
@@ -52,7 +52,7 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
 
 
 
-modified_board = flood_fill(input_board=board, old=".", new="~", x=0, y=0)
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
 
 for a in modified_board:
     print(a)
